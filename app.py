8582ee10eb2640c39d77b8998ff29dab1eb42fe2
rachelhollis <rachel.hollis@live.com>
2022-02-02 10:55:31 -0600

    challenge

--- app.py
+++ app.py
@@ -5,11 +5,65 @@ Created on Wed Jan 12 19:42:40 2022
 @author: rache
 """
 
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#    return 'Hello World'
+
+# import pandas datetime and numpy
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+# import sqlalchemy 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# import dependency for Flask
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route("/")
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+            /api/v1.0/precipitation
+            /api/v1.0/stations
+            /api/v1.0/tobs
+            /api/v1.0/temp/start/end
+        ''')
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+
+
+
+
+
+
+
 
